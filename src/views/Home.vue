e919b6a03d5cc08d8f6c8bd81b98e11de0f102ea
chendalin666 <1255925603@qq.com>
2021-03-17 17:19:23 +0800

    '1'

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" ref="hw" @show="show($event)"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  provide: {
+    data: 'hello vue-cli'
+  },
+  components: {
+    HelloWorld
+  },
+  methods: {
+    show (e) {
+      console.log('show方法被调用' + e)
+    }
+  },
+  mounted () {
+    console.log(this)
+    this.$refs.hw.foo1 = 'oof'
+    this.$children[0].foo2 = '111'
+  }
+}
+</script>
